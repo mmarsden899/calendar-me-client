9488ea61e832beee871779434d22dc695d6af76b
mmarsden89 <mmarsden89@gmail.com>
2020-04-02 15:06:00 -0400

    Add functionality for back and forward buttons. Add scss file and added sass-node dependecy

--- src/Calendar/CalendarView.js
+++ src/Calendar/CalendarView.js
@@ -1,79 +1,97 @@
-import React, { useState, useEffect } from 'react'
-import moment from 'moment'
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import "./CalendarView.scss";
+import moment from "moment";
 
 const CalendarView = () => {
+  const [currentDate, setCurrentDate] = useState(moment());
+  const [dateObject, setDateObject] = useState(moment());
 
-    // const [currentDate, setCurrentDate] = useState({})
-    const [dateObject, setDateObject] = useState(moment())
+  const firstDay = () => {
+    let first = moment(dateObject)
+      .startOf("month")
+      .format("d");
+    return first;
+  };
 
-    useEffect(() => {
-        const loadDefaultData = async () => {
-            // const current = moment()
-            // setCurrentDate(current)
-        }
-        loadDefaultData()
-    }, [])
+  let blankDays = [];
+  for (let i = 0; i < firstDay(); i++) {
+    blankDays.push(<td className="calendar-day empty">{""}</td>);
+  }
+  let days = [];
+  for (let k = 1; k <= moment(dateObject).daysInMonth(); k++) {
+    days.push(
+      <td key={k} className="calendar-day">
+        {k}
+      </td>
+    );
+  }
 
-    const firstDay = () => {
-        let first = moment(dateObject).startOf('month').format('d')
-        return first
+  const weekdayShort = moment.weekdaysShort().map(day => {
+    return <th key={day}>{day}</th>;
+  });
+
+  var totalSlots = [...blankDays, ...days];
+  let rows = [];
+  let cells = [];
+
+  totalSlots.forEach((row, i) => {
+    if (i % 7 !== 0) {
+      cells.push(row); // if index not equal 7 that means not go to next week
+    } else {
+      rows.push(cells); // when reach next week we contain all td in last week to rows
+      cells = []; // empty container
+      cells.push(row); // in current loop we still push current row to new container
     }
-
-    let blankDays = []
-    for (let i = 0; i < firstDay(); i++) {
-        blankDays.push(
-        <td className="calendar-day empty">{''}</td>
-        )
+    if (i === totalSlots.length - 1) {
+      // when end loop we add remain date
+      rows.push(cells);
     }
-    let days = [];
-    for (let k = 1; k <= moment().daysInMonth(); k++) {
-        days.push(
-            <td key={k} className="calendar-day">
-          {k}
-        </td>
-      );
-    }
-    
-    const weekdayShort = moment.weekdaysShort().map(day => {
-        return (
-            <th key={day}>{day}</th>
-        )
-    })
+  });
 
-    var totalSlots = [...blankDays, ...days];
-    let rows = [];
-    let cells = [];
+  let daysInMonth = rows.map((d, i) => {
+    return <tr>{d}</tr>;
+  });
 
-    totalSlots.forEach((row, i) => {
-        if (i % 7 !== 0) {
-          cells.push(row); // if index not equal 7 that means not go to next week
-        } else {
-          rows.push(cells); // when reach next week we contain all td in last week to rows 
-          cells = []; // empty container 
-          cells.push(row); // in current loop we still push current row to new container
-        }
-        if (i === totalSlots.length - 1) { // when end loop we add remain date
-          rows.push(cells);
-        }
-      });
+  const handleMonthBack = () => {
+    const newDate = moment(dateObject).subtract(1, "months");
+    setDateObject(newDate);
+  };
 
-    let daysInMonth = rows.map((d, i) => {
-        return <tr>{d}</tr>;
-      });
+  const handleMonthForward = () => {
+    const newDate = moment(dateObject).add(1, "months");
+    setDateObject(newDate);
+  };
 
-      const defaultCalendarMap = (
-        <table className="calendar-day">
+  const header = (
+    <div>
+      <FontAwesomeIcon
+        icon={faArrowLeft}
+        onClick={handleMonthBack}
+        className="hover"
+      />
+      <FontAwesomeIcon
+        icon={faArrowRight}
+        onClick={handleMonthForward}
+        className="hover"
+      />
+      {moment(dateObject).format("MMMM YYYY")}
+    </div>
+  );
+
+  const defaultCalendarMap = (
+    <div>
+      {header}
+      <table className="calendar-day">
         <thead>
           <tr>{weekdayShort}</tr>
         </thead>
         <tbody>{daysInMonth}</tbody>
       </table>
-      )
-    return (
-        <div className="calendar-view-container">
-            {defaultCalendarMap}
-        </div>
-    )
-}
+    </div>
+  );
+  return <div className="calendar-view-container">{defaultCalendarMap}</div>;
+};
 
-export default CalendarView
+export default CalendarView;
