fd2218b55125a84b1503d9da26eeafd9fc39bf04
mmarsden89 <mmarsden89@gmail.com>
2020-04-04 18:41:42 -0400

    Add auth0 to project. Currently working locally as of this commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import './App.css';
-import CalendarView from './Calendar/CalendarView'
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import CalendarView from "./Calendar/CalendarView";
+import NavBar from "./Components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
+import Profile from "./Components/Profile";
+import history from "./Utils/History";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="App">
-      {/* <header className="App-header">
-      </header> */}
-      <CalendarView/>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact component={CalendarView} />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+        {/* <CalendarView /> */}
+      </Router>
     </div>
   );
 }
